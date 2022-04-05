bc1367ec8c7a5f24b2c2696a5c1d90cd04173236
radityamaualana <93692780+aditmaulana090504@users.noreply.github.com>
2022-04-05 12:03:03 +0700

    Add files via upload

---
+++ adit/maulana/admin/config/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('baseurl','http://localhost/adit/maulana/public');
+
+//DB
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','latihan_ukkadit09');
